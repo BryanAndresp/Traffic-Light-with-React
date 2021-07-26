d85b32e30ed02aa23b8592ba9a6df2675f6d74c0
omegamaximus10 <bryan_2904@hotmail.com>
2021-07-26 16:04:11 +0000

    ejercicio finalizado

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Navbar } from "./navbar.js";
-import { Jumbotron } from "./jumbotron.js";
-import { Container } from "./container";
-import { Footer } from "./footer.js";
-
+//create your first component
 export function Home() {
+	const [btnRed, setBtnRed] = useState("");
+	const [btnYellow, setBtnYellow] = useState("");
+	const [btnGreen, setBtnGreen] = useState("");
+
+	const handleClickRed = () => {
+		setBtnYellow(""), setBtnRed("glow"), setBtnGreen("");
+	};
+
+	const handleClickYellow = () => {
+		setBtnYellow("glow "), setBtnRed(""), setBtnGreen("");
+	};
+	const handleClickGreen = () => {
+		setBtnYellow(""), setBtnRed(""), setBtnGreen("glow");
+	};
+
 	return (
-		<div>
-			<Navbar />
-			<Jumbotron />
-			<Container />
-			<Footer />
-		</div>
+		<>
+			<div className="bar"></div>
+			<div className="traffic-light">
+				<div
+					onClick={handleClickRed}
+					className={`light red ${btnRed}`}></div>
+				<div
+					onClick={handleClickYellow}
+					className={`light yellow ${btnYellow}`}></div>
+				<div
+					onClick={handleClickGreen}
+					className={`light green ${btnGreen}`}></div>
+			</div>
+		</>
 	);
 }
